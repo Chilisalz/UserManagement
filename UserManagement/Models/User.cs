060be5b3de029342826d2fe46b5e736a6f6814c5
Jan-Lukas Spilles <jan-lukasspilles@web.de>
2021-11-06 11:48:18 +0100

    Test

--- UserManagement/Models/User.cs
+++ UserManagement/Models/User.cs
@@ -7,6 +7,9 @@ namespace Benutzerverwaltung.Models
 {
     public class User
     {
+        public Guid Id { get; set; }
         public string Username { get; set; }        
+        public string Email { get; set; }
+        public DateTime RegistrationDate { get; set; }        
     }
 }
