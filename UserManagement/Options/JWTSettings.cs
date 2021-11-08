601f30977f1325884a8bd213fea37c9e50227659
Jan-Lukas Spilles <jan-lukasspilles@web.de>
2021-11-08 16:02:20 +0100

    unnötige usings entfernt und jwtsettings verschoben

--- UserManagement/Options/JWTSettings.cs
+++ UserManagement/Options/JWTSettings.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace UserManagementService.Authentication
+namespace UserManagementService.Options
 {
     public class JWTSettings
     {
